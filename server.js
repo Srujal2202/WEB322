c8b0835c9736290b212c9bfddf17ad920d45884a
Srujal2202 <164557874+Srujal2202@users.noreply.github.com>
2024-03-24 23:13:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,11 @@
  * I declare that this assignment is my own work in accordance with Seneca's
  * Academic Integrity Policy:
  *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html 
- * Name: Srujal Patel       Student ID: 182573212    Date: 2024-3-19
+ * Name: Srujal Patel  Student ID: 182573212   Date: 2024-3-19
  * *******************************************************************************/
-
 const legoData = require("./modules/legoSets");
+
+
 const path = require("path");
 
 const express = require('express');
@@ -24,43 +25,35 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/about', (req, res) => res.render('about'));
 
 app.get("/lego/sets", async (req,res)=>{
-  try
-  {
-    if(req.query.theme)
-    {
+  try{
+    if(req.query.theme){
       let sets = await legoData.getSetsByTheme(req.query.theme);
       res.render("sets", { sets: sets });
   
-    }else
-    {
+    }else{
       let sets = await legoData.getAllSets();
       res.render("sets", { sets: sets });
     }
-  }catch(err)
-  {
+  }catch(err){
     res.status(404).render("404", { message: "Unable to find requested set." });
   }
 
 });
 
 app.get("/lego/sets/:num", async (req,res)=>{
-  try
-  {
+  try{
     let set = await legoData.getSetByNum(req.params.num);
     res.render("set", {set: set});
-  }catch(err)
-  {
+  }catch(err){
     res.status(404).render("404", { message: "Unable to find requested sets." });
   }
 });
 
-app.use((req, res, next) => 
-{
+app.use((req, res, next) => {
   res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for." });
 });
 
 
-legoData.initialize().then(()=>
-{
+legoData.initialize().then(()=>{
   app.listen(HTTP_PORT, () => { console.log(`server running on: http://localhost:${HTTP_PORT}`) });
 });
